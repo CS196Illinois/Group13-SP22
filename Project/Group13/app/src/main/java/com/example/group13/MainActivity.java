7629125986f3bb98dc003e8bc40361a887ebca10
ivanvaldez3 <ivalde5@illinois.edu>
2022-04-30 16:58:58 -0500

    UI Design Updates

--- Project/Group13/app/src/main/java/com/example/group13/MainActivity.java
+++ Project/Group13/app/src/main/java/com/example/group13/MainActivity.java
@@ -1,40 +1,31 @@
 package com.example.group13;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.chaquo.python.PyObject;
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.material.button.MaterialButton;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -42,13 +33,12 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.types.Track;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] items;
 
     private static final String CLIENT_ID = "73ce87ec19814717914402e26595bdfe";
     private static final String REDIRECT_URI = "com.example.group13://callback";
@@ -58,13 +48,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Implement OnCreate
+        listView = (ListView)findViewById(R.id.listViewSong);
+
+        runtimePermission();
+
+        TextView playlist = (TextView) findViewById(R.id.username);
+        Spinner mood = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.drop_down, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mood.setAdapter(adapter);
+
+        mood.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView)adapterView.getChildAt(0)).setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        MaterialButton btn = (MaterialButton) findViewById(R.id.btn);
+        //; here
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Error in the name function
+                if(mood!= null && mood.getSelectedItem() != null) {
+                    NextActivity(view);
+                } else {
+
+                }
+            }
+            public void NextActivity(View view) {
+                Intent i = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        TextView text = findViewById(R.id.test);
-        text.setText("test");
 
         ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
@@ -79,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
                         mSpotifyAppRemote = spotifyAppRemote;
                         Log.d("MainActivity", "Connected! Yay!");
 
-                        text.setText("connected");
                         // Now you can start interacting with App Remote
                         connected();
 
@@ -90,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
 
                         // Something went wrong when attempting to connect! Handle errors here
 
-                        text.setText("connection failure");
                     }
                 });
 
@@ -116,6 +143,92 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+    //The Music Player UI
+    public void runtimePermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        //displaySongs();
+                    }
 
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
 
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+    /*public ArrayList<File> findSong (File file)
+    {
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        for (File singlefile: files)
+        {
+            if (singlefile.isDirectory() && !singlefile.isHidden())
+            {
+                arrayList.addAll(findSong(singlefile));
+            }
+            else
+            {
+                if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav"))
+                {
+                    arrayList.add(singlefile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void displaySongs() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+        for (int i = 0; i < mySongs.size(); i++)
+        {
+            //Might be an error here
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(myAdapter);
+
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class customAdapter extends BaseAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            //Might be an issue
+            View myView = getLayoutInflater().inflate(R.layout.activity_final, null);
+            TextView textsong = myView.findViewById(R.id.txtsongname);
+            textsong.setSelected(true);
+            textsong.setText(items[i]);
+
+            return myView;
+        }
+    }*/
 }
---
+++ Project/Group13/app/src/main/java/com/example/group13/NextActivity.java
@@ -0,0 +1,15 @@
+package com.example.group13;
+
+import androidx.annotation.Nullable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class  NextActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
